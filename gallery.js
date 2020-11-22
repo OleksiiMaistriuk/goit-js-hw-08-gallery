f9125a88d802265a8314f256725bd29a914cb15d
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-11-22 16:49:00 +0100

    закончил дз

--- gallery.js
+++ gallery.js
@@ -1,59 +1,51 @@
 import galleryItem from './gallery-items.js';
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-
-// Открытие модального окна по клику на элементе галереи.
-
-// Подмена значения атрибута src элемента img.lightbox__image.
-
-// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-
-// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
-// чтобы при следующем открытии модального окна, пока грузится изображение,
-// мы не видели предыдущее.
-
-/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li>; */
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
 const refs = {
   galleryList: document.querySelector('.js-gallery'),
   lightbox: document.querySelector('.js-lightbox'),
   lightboxImg: document.querySelector('.lightbox__image'),
   closeBtn: document.querySelector('.lightbox__button'),
-  overlay: document.querySelector('.lightbox__overlay'),
 };
-const createGalleryList = document.createElement('li');
-createGalleryList.classList.add('gallery__item');
 
-const createGalleryLink = document.createElement('a');
-createGalleryLink.classList.add('gallery__link"');
-createGalleryLink.href =
-  'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg';
+const createGalleryItem = picture => {
+  const createGalleryList = document.createElement('li');
+  createGalleryList.classList.add('gallery__item');
+  const createGalleryLink = document.createElement('a');
+  createGalleryLink.classList.add('gallery__link"');
+  createGalleryLink.href = `${picture.original}`;
+  const createGalleryImg = document.createElement('img');
+  createGalleryImg.classList.add('gallery__image');
+  createGalleryImg.src = `${picture.preview}`;
+  createGalleryImg.alt = `${picture.description}`;
+  createGalleryImg.dataset.source = ` ${picture.original}`;
+  createGalleryList.append(createGalleryLink, createGalleryImg);
+  return createGalleryList;
+};
 
-const createGalleryImg = document.createElement('img');
-createGalleryImg.classList.add('gallery__image');
-createGalleryImg.src =
-  'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg';
-createGalleryImg.alt = 'Tulips';
-createGalleryImg.dataset.source =
-  'https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg';
+const galleryCard = galleryItem.map(card => createGalleryItem(card));
+refs.galleryList.append(...galleryCard);
+refs.galleryList.addEventListener('click', onTagsClick);
+refs.closeBtn.addEventListener('click', removeLightboxLink);
 
-createGalleryList.append(createGalleryLink, createGalleryImg);
+function onTagsClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  refs.lightbox.classList.add('is-open');
+  refs.lightboxImg.src = event.target.getAttribute('data-source');
+  refs.lightboxImg.alt = event.target.alt;
+  refs.lightboxImg.src = event.target.dataset.source;
+  window.addEventListener('keyup', clickEscape);
+}
 
-refs.galleryList.appendChild(createGalleryList);
+function removeLightboxLink() {
+  refs.lightbox.classList.remove('is-open'),
+    (refs.lightboxImg.src = ''),
+    (refs.lightboxImg.alt = '');
+}
 
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-console.log(refs.galleryList);
+function clickEscape(event) {
+  if (event.code === 'Escape') {
+    removeLightboxLink();
+  }
+}
